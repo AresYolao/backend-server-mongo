9b8a64b7eee99420566a525523998ffe721f3049
Ares Garcia <agarciap@ids.com.mx>
2020-04-27 22:45:11 -0500

    primer commit

---
+++ app.js
@@ -0,0 +1,26 @@
+// Requires
+var express = require('express');
+var mongoose = require('mongoose')
+
+//inicializar variables
+var app = express();
+
+//Conexión a la base de datos
+mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, resp) => {
+    if (err) throw err;
+    console.log('Base de datos: \x1b[32m%s\x1b[0m', ' online');
+
+})
+
+//Rutas
+app.get('/', (request, response, next) => {
+    response.status(200).json({
+        ok: true,
+        mensaje: 'petición realizada correctamente'
+    })
+})
+
+//Escuchar peticiones
+app.listen(3000, () => {
+    console.log('Express server puerto 3000: \x1b[32m%s\x1b[0m', ' online');
+})
